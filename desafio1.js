6e5ab4ba2c4d64db80b14de96d044a6a8273b4cb
wato-kado <wato.kado@gmail.com>
2021-09-11 03:08:30 -0300

    Desafio1

---
+++ desafio1.js
@@ -0,0 +1,15 @@
+myFunction();
+
+function myFunction() {
+  var nombre = "Carlos";
+  alert("Hola " + nombre);
+}
+
+//otra opción para mejorarlo
+
+miFuncion();
+
+function miFuncion() {
+  var nombre = prompt("¿Cuál es su nombre?")
+  alert("Hola " + nombre);
+}
